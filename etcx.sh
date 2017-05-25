2a3cddcf20e90a1d84a2f59c2cbf3c548509c2db
Thomas Maier <contact@thomas-maier.net>
2017-05-25 22:28:39 +0200

    WiP

--- etcx.sh
+++ etcx.sh
@@ -1,74 +1,60 @@
 #!/usr/bin/env bash
 
-function check_user_exists {
+function install_etcx_file {
+    owner=`echo $1 | grep -Po '[^.]+(?=@[^@]+$)'`
+    file=`echo $1 | grep -Po '[^.].+(?=.etcx.*$)'`
+    mode=`stat -c '%a' $1`
+
+    echo $owner
+    echo $file
+    echo $mode
+
+    return
+
     getent passwd $1 > /dev/null
-}
+    if [ $? -ne 0 ]; then return; fi
 
-function check_group_exists {
     getent group $1 > /dev/null
-}
+    if [ $? -ne 0 ]; then return; fi
 
-function get_hostname {
-    echo `hostname`
-}
+    if [ ! -d `dirname $target_file` ]; then return; fi
 
-function extract_username {
-    echo $1 | grep -Po '[^.]+(?=@[^@]+$)'
-}
+    cmd="install --no-target-directory --owner=$owner --group=$owner --mode='$target_mode' $1 $target_file"
 
-function extract_hostname {
-    echo $1 | grep -Po '[^@]+$'
-}
-
-function extract_targetfile {
-    echo $1 | grep -Po '[^.].+(?=.etcx.*$)'
-}
-
-function get_source_mode {
-    stat -c '%a' $1
-}
-
-function install_file {
-    owner=`extract_username $1`
-    target_file=`extract_targetfile $1`
-    target_mode=`get_source_mode $1`
-
-    check_user_exists $owner
-    if [ $? -ne 0 ]; then
-        echo unknown user $owner
-        return
+    if [ -n $DRYRUN ]; then
+        echo $cmd
+    else
+        eval $cmd
     fi
-
-    check_group_exists $owner
-    if [ $? -ne 0 ]; then
-        echo unknown group $owner
-        return
-    fi
-
-    if [ ! -d `dirname $target_file` ]; then
-        echo missing target folder for $target_file
-        return
-    fi
-
-    cmd="install --no-target-directory --owner=${owner} --group=${owner} --mode='${target_mode}' ${1} ${target_file}"
-    echo $cmd
-    eval $cmd
 }
 
-function main {
-    cd fsroot
+USAGE="$0 [-d] [-s sourcedir]
+-d dryrun (just prints the install commands)
+-s source directory (defaults to ./etc)
+"
 
-    for file in `find . -type f -name '*@all'`; do
-        install_file $file
-    done
+while getopts "hds:" opt; do
+    case ${opt} in
+        h ) printf "$USAGE" && exit 0;;
+        d ) DRYRUN=1;;
+        s ) SOURCE_DIR=$OPTARG;;
+    esac
+done
 
-    hostname=`get_hostname`
-    for file in `find . -type f -not -name '*@all'`; do
-        for_host=`extract_hostname $file`
-        if [ "$hostname" = "$for_host" ]; then
-            install_file $file
-        fi
-    done
-}
+if [ -z $SOURCE_DIR ]; then SOURCE_DIR=./etc; fi
 
-main
+cd $SOURCE_DIR
+
+for file in `find . -type f -name '*.etcx.*@all'`; do
+    install_etcx_file $file
+done
+
+exit 0
+
+hostname=`hostname`
+for file in `find . -type f -not -name '*@all'`; do
+    for_host=`echo $file | grep -Po '[^@]+$'`
+    if [ "$hostname" = "$for_host" ]; then
+        install_etcx_file $file
+    fi
+done
